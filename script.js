8b9e2320202c71179ed3eb00eb073914df1e89e6
Adil <89954695+programest@users.noreply.github.com>
2024-01-17 13:31:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -62,16 +62,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-  function redirectToWhatsApp() {
-    // Замените "Ваш текст для сообщения" на текст, который вы хотите отправить в WhatsApp
-    var message = encodeURIComponent("Ваш текст для сообщения");
-
-    // Замените "+123456789" на номер WhatsApp, на который вы хотите отправить сообщение
-    var phoneNumber = "+7(771)411-07-05";
-
-    // Собираем URL для перехода на WhatsApp с предварительно заданным сообщением
-    var whatsappURL = "https://wa.me/" + phoneNumber + "?text=" + message;
-
-    // Перенаправляем пользователя на WhatsApp
-    window.location.href = whatsappURL;
-}
+function redirectToWhatsApp() {
+      // Замените "Ваш текст для сообщения" на текст, который вы хотите отправить в WhatsApp
+      var message = encodeURIComponent("Ваш текст для сообщения");
+  
+      // Замените "+123456789" на номер WhatsApp, на который вы хотите отправить сообщение
+      var phoneNumber = "+77714110705";
+  
+      // Проверяем, является ли устройство мобильным
+      var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  
+      // Собираем URL для перехода на WhatsApp с предварительно заданным сообщением
+      var whatsappURL;
+  
+      if (isMobile) {
+          whatsappURL = "https://wa.me/" + phoneNumber + "?text=" + message;
+      } else {
+          // Если устройство не мобильное, открываем веб-версию WhatsApp
+          whatsappURL = "https://web.whatsapp.com/send?phone=" + phoneNumber + "&text=" + message;
+      }
+  
+      // Открываем WhatsApp в новой вкладке
+      window.open(whatsappURL, '_blank');
+  }
